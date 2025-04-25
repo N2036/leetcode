0efe319f9326d544d2d734b4508d336e12b55951
N2036 <sreen2981@gmail.com>
2025-04-25 18:47:38 +0530

    Time: 3 ms (99.88%) | Memory: 56.7 MB (52.86%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.java
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.java
@@ -1,33 +1,16 @@
-import java.util.*;
-
-public class Solution {
+class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
-        List<Integer> result = new ArrayList<>();
-        for (int i = 0; i < nums.length; i++) {
-            int idx = Math.abs(nums[i]) - 1;  
-            if (nums[idx] > 0) {
-                nums[idx] = -nums[idx];
-            }
-        }
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] > 0) {
-                result.add(i + 1);
-            }
-        }
+    int[] res = new int[nums.length + 1];
+	for (int num : nums) {
+		res[num] = num;
+	}
 
-        return result;
-    }
-
-    public static void main(String[] args) {
-        Solution sol = new Solution();
-
-        int[] nums1 = {4,3,2,7,8,2,3,1};
-        System.out.println("Input:  " + Arrays.toString(nums1));
-        System.out.println("Output: " + sol.findDisappearedNumbers(nums1));  
-
-        int[] nums2 = {1,1};
-        System.out.println("Input:  " + Arrays.toString(nums2));
-        System.out.println("Output: " + sol.findDisappearedNumbers(nums2)); 
+	List<Integer> result = new ArrayList<>();
+	for (int i = 1; i <= nums.length; i++) {
+		if (res[i] == 0) {
+			result.add(i);
+		}
+	}
+	return result;
     }
 }
-
