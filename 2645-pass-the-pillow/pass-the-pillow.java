f423dd4769beac46ecd5b5e86d8b70dc79de65b0
N2036 <sreen2981@gmail.com>
2025-05-06 20:45:25 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (69.40%) - LeetSync

---
+++ 2645-pass-the-pillow/pass-the-pillow.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int passThePillow(int n, int time) {
+        if(time < n) return time+1;
+        boolean start=true;
+        while(time>=n){
+            time-=n-1;
+            start=!start;
+        }
+        if(start) return time+1;
+        else return n-time;
+    }
+}
