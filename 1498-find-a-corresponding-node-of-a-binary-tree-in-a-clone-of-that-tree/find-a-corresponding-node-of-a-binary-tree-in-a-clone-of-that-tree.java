970208af88684293693d224f02f8a127aa91452d
N2036 <sreen2981@gmail.com>
2025-05-28 10:25:05 +0530

    Time: 1 ms (97.98%) | Memory: 49.7 MB (60.59%) - LeetSync

--- 1498-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.java
+++ 1498-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.java
@@ -10,13 +10,10 @@
 
 class Solution {
     public final TreeNode getTargetCopy(final TreeNode original, final TreeNode cloned, final TreeNode target) {
-        if(original==null) return null;
-        if(original==target) return cloned;
-        TreeNode left=getTargetCopy(original.left,cloned.left,target);
-        if(left!=null){
-            return left;
-        }else{
-            return getTargetCopy(original.right,cloned.right,target);
-        }
+        if(original == null) return null;
+        if(original == target) return cloned;
+        TreeNode left= getTargetCopy(original.left,cloned.left,target);
+        if(left!=null) return left;
+        return getTargetCopy(original.right,cloned.right,target);
     }
 }
